db7bd5e1defb700a16480bf1b40d35a5ab2c64d7
mario arriola <riblarsv@gmail.com>
2023-07-21 03:13:18 +0200

    first commit

---
+++ votes/src/data/data.js
@@ -0,0 +1,17 @@
+export const myTeams = [
+    {
+        name:'Barcelona',
+        img:'https://banner2.cleanpng.com/20180921/eja/kisspng-fc-barcelona-football-logo-la-liga-vector-graphics-como-tener-equipo-del-fc-barcelona-1-dream-lea-5ba52ef8ebde17.6137999715375521209661.jpg',
+        votes:0
+    },
+    {
+        name:'Real',
+        img:'https://banner2.cleanpng.com/20180921/eja/kisspng-fc-barcelona-football-logo-la-liga-vector-graphics-como-tener-equipo-del-fc-barcelona-1-dream-lea-5ba52ef8ebde17.6137999715375521209661.jpg',
+        votes:0
+    },
+    {
+        name:'Valencia',
+        img:'https://banner2.cleanpng.com/20180921/eja/kisspng-fc-barcelona-football-logo-la-liga-vector-graphics-como-tener-equipo-del-fc-barcelona-1-dream-lea-5ba52ef8ebde17.6137999715375521209661.jpg',
+        votes:0
+    }
+]
